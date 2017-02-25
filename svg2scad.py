526d975d25b2b86d386b8f6d6caecc1f41279370
arpruss <arpruss@gmail.com>
2017-02-25 09:34:50 -0600

    foo

--- svg2scad.py
+++ svg2scad.py
@@ -7,12 +7,17 @@ from inflateutils.exportmesh import *
 
 quiet = False
 
-def getBounds(lines):
-    bottom = min(min(l[0].imag,l[1].imag) for l in lines)
-    left = min(min(l[0].real,l[1].real) for l in lines)
-    top = max(max(l[0].imag,l[1].imag) for l in lines)
-    right = max(max(l[0].real,l[1].real) for l in lines)
-    return left,bottom,right,top
+def updateBounds(bounds,points):
+    bounds[0] = min(bounds[0], min(p.real for p in points))
+    bounds[1] = min(bounds[1], min(p.imag for p in points))
+    bounds[2] = max(bounds[0], max(p.real for p in points))
+    bounds[3] = max(bounds[1], max(p.imag for p in points))
+    
+def getCenterX(bounds):
+    return 0.5*(bounds[0]+bounds[2])
+
+def getCenterY(bounds):
+    return 0.5*(bounds[1]+bounds[3])
 
 def message(string):
     if not quiet:
@@ -28,37 +33,35 @@ def sortedApproximatePaths(paths,error=0.1):
         
     return sorted(paths, key=key)
 
-class ExtractedData(object):
-    def __init__(self):
-        self.pathData = {}
-        self.polygon = {}
-    
-class PathData(object):
-    def __init__(self,points,fillColor,edgeColor):
-        self.points = points
+class PolygonData(object):
+    def __init__(self,fillColor,edgeColor):
+        self.bounds = [float("inf"),float("inf"),float("-inf"),float("-inf")]
         self.fillColor = fillColor
         self.edgeColor = edgeColor
+        self.pointLists = []
     
 def extractSVGFile(svgFile, tolerance=0.1, baseName="svg"):
-    data = ExtractedData()
+    polygons = []
 
     paths = sortedApproximatePaths( parser.getPathsFromSVGFile(svgFile)[0], error=tolerance )
     
     for i,path in enumerate(paths):
-        polyName = str(i)
-        data.polygon[polyName] = []
-        subpaths = []
+        polygon = PolygonData(path.svgState.fill,path.svgState.stroke)
+        polygons.append(polygon)
         for j,subpath in enumerate(path.breakup()):
             points = [subpath[0].start]
             for line in subpath:
                 points.append(line.end)
             if subpath.closed and points[0] != points[-1]:
                 points.append(points[0])
-            name = str(i) + "_" + str(j)
-            data.pathData[name] = PathData(points,path.svgState.fill,path.svgState.stroke)
-            data.polygon[polyName].append(name)
+            updateBounds(polygon.bounds,points)
+            polygon.pointLists.append(points)
+        polygon.center = complex(0.5*(polygon.bounds[0]+polygon.bounds[2]),0.5*(polygon.bounds[1]+polygon.bounds[3]))
+        for points in polygon.pointLists:
+            for j in range(len(points)):
+                points[j] -= polygon.center
             
-    return data
+    return polygons
     
 def describeColor(c):
     if c is None:
@@ -119,7 +122,7 @@ if __name__ == '__main__':
         help(exitCode=1)
         sys.exit(2)
         
-    data = extractSVGFile(args[0], tolerance=tolerance, baseName=baseName)
+    polygons = extractSVGFile(args[0], tolerance=tolerance, baseName=baseName)
     
     scad = ""
     if (mode.startswith("pol") or mode[0] == "r") and height > 0:
@@ -127,13 +130,26 @@ if __name__ == '__main__':
     if mode[0] == "r":
         scad += "width_%s = %.9f;\n" % (baseName, width)
         
-    for name in sorted(data.pathData):
-        d = data.pathData[name]
-        scad += "points_" + baseName+ "_" + name + " = [ " + ','.join('[%.9f,%.9f]' % (point.real,point.imag) for point in d.points) + " ];\n"
-        scad += "fill_color_" + baseName + "_" + name + " = " + describeColor(d.fillColor) + ";\n"
-        scad += "outline_color_" + baseName + "_" + name + " = " + describeColor(d.edgeColor) + ";\n"
+    if len(scad):
         scad += "\n"
         
+    def polyName(i):
+        return baseName + "_" + str(i+1)
+        
+    def subpathName(i,j):
+        return polyName(i) + "_" + str(j+1)
+        
+    for i,polygon in enumerate(polygons):
+        scad += "center_%s = [%.9f,%.9f];\n" % (polyName(i), polygon.center.real, polygon.center.imag)
+        scad += "fill_color_%s = %s;\n" % (polyName(i), describeColor(polygon.fillColor))
+        scad += "outline_color_%s = %s;\n\n" % (polyName(i), describeColor(polygon.edgeColor))
+        
+    for i,polygon in enumerate(polygons):
+        scad += "// paths for %s\n" % polyName(i)
+        for j,points in enumerate(polygon.pointLists):
+            scad += "points_" + subpathName(i,j) + " = [ " + ','.join('[%.9f,%.9f]' % (point.real,point.imag) for point in points) + " ];\n"
+        scad += "\n"
+
     if mode[0] == "r":
         scad += """module ribbon(points, thickness=width_%s) {
     p = points;
@@ -149,34 +165,45 @@ if __name__ == '__main__':
 }
 
 """ % (baseName,)
+
+        objectName = "ribbon"
         
-        for name in sorted(data.pathData):
-            if data.pathData[name].edgeColor is not None:
-                scad += "color(outline_color_%s_%s) " % (baseName,name)
-            elif data.pathData[name].fillColor is not None:
-                scad += "color(fill_color_%s_%s) " % (baseName,name)
+        for i,polygon in enumerate(polygons):
+            scad += "module %s_%s() {\n " % (objectName,polyName(i),)
+            if polygon.edgeColor is not None:
+                scad += "color(outline_color_%s) " % (polyName(i),)
+            elif polygon.fillColor is not None:
+                scad += "color(fill_color_%s) " % (polyName(i),)
                 
-        if height > 0:
-            scad += "linear_extrude(height=height_%s) " % (baseName,)
-        scad += "ribbon(points_%s_%s, thickness=width_%s);\n\n" % (baseName, name, baseName)
+            if height > 0:
+                scad += "linear_extrude(height=height_%s) " % (baseName,)
+            scad += "{\n"
+            for j in range(len(polygon.pointLists)):
+                scad += "  ribbon(points_%s, thickness=width_%s);\n" % (subpathName(i,j), baseName)
+            scad += " }\n}\n\n"
     elif mode.startswith("pol"):
-        for name in sorted(data.polygon):
-            scad += "module polygon_%s_%s() {\n" % (baseName, name)
-            for subname in data.polygon[name]:
-                scad += " polygon(points=points_%s_%s);\n" % (baseName,subname)
-            scad += "}\n\n"
+    
+        objectName = "polygon"
+    
+        for i,polygon in enumerate(polygons):
+            scad += "module %s_%s() {\n " % (objectName,polyName(i),)
+            if polygon.fillColor is not None:
+                scad += "color(fill_color_%s) " % (polyName(i),)
+            elif d.edgeColor is not None:
+                scad += "color(outline_color_%s) " % (polyName(i),)
+            if height > 0:
+                scad += "linear_extrude(height=height_%s) " % (baseName,)
+            scad += "{\n"
+            for j in range(len(polygon.pointLists)):
+                scad += "  polygon(points=points_%s);\n" % subpathName(i,j)  
+            scad += " }\n}\n\n"
             
-        for name in sorted(data.polygon):
-            if data.polygon[name]:
-                firstName = data.polygon[name][0]
-                d = data.pathData[firstName]
-                if d.fillColor is not None:
-                    scad += "color(fill_color_%s_%s) " % (baseName,firstName)
-                elif d.edgeColor is not None:
-                    scad += "color(outline_color_%s_%s) " % (baseNane,firstName)
-                if height > 0:
-                    scad += "linear_extrude(height=height_%s) " % (baseName,)
-                scad += "polygon_%s_%s();\n\n" % (baseName,name)
+    else:
+        objectName = None
+        
+    if objectName is not None:
+        for i in range(len(polygons)):
+            scad += "translate(center_%s) %s_%s();\n" % (polyName(i), objectName, polyName(i))
             
     if outfile:
         with open(outfile, "w") as f: f.write(scad)
